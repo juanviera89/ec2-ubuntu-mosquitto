985dd3599878a070d584c8cc77745c50f26f2c38
Juan Viera (PC Tarro) <juanviera89@gmail.com>
2025-03-05 00:24:12 -0300

    pass file permissions

--- scripts/mqtt_update_pass_sub.sh
+++ scripts/mqtt_update_pass_sub.sh
@@ -10,5 +10,6 @@ MQTT_SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id "$MQTT_SECRE
 
 MQTT_PASS=$(echo "$MQTT_SECRET_VALUE" | jq -r '.["mqtt-pass"]')
 echo "$MQTT_PASS" > "/etc/mosquitto/pass.txt"
+chmod 0700 /etc/mosquitto/pass.txt
 mosquitto_passwd -U /etc/mosquitto/pass.txt
 exit 0
