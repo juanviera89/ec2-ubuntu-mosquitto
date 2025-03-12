f956a5f96adface02ab9e9f776e5f002b9d8489a
Juan Viera (PC Tarro) <juanviera89@gmail.com>
2025-03-11 23:48:20 -0300

    fix intial parameters on manager

--- mqtt_manager.sh
+++ mqtt_manager.sh
@@ -4,35 +4,38 @@ case "$1" in
   "reparar_permisos")
     chmod +x ./scripts/*.sh
     ;;
-  "configurar_cron")
-    ./scripts/configurar_cron.sh
+  "configurar_limpieza")
+    ./scripts/configurar_cron.sh "$2"
     ;;
   "configurar_logrotate")
     ./scripts/configurar_logrotate.sh
-    ;;
-  "configurar_mosquito_health")
-    ./scripts/configurar_cron_mosquitto.sh "$2"
     ;;
   "instalacion_inicial")
     ./scripts/instalacion_inicial.sh "$2"
     ;;
   "instalar_awscli")
-    ./scripts/instalar_awscli.sh
+    ./scripts/instalar_awscli.sh "$2"
     ;;
   "instalar_mqtt")
     ./scripts/instalar_mqtt.sh "$2"
     ;;
   "mqtt_update_config")
-    ./scripts/mqtt_update_config.sh
+    ./scripts/mqtt_update_config.sh "$2"
     ;;
   "mqtt_update_pass")
-    ./scripts/mqtt_update_pass.sh
+    ./scripts/mqtt_update_pass.sh "$2"
     ;;
   "mqtt_server_diagnosis")
     ./scripts/mqtt_server_diagnosis.sh "$2"
     ;;
+  "configurar_monitor")
+    ./scripts/configurar_mosquito_health.sh "$2"
+    ;;
+  "configurar_reinicio")
+    ./scripts/configurar_mqtt_cron.sh "$2"
+    ;;
   *)
-    echo "Uso: $0 {reparar_permisos|configurar_cron|configurar_logrotate|configurar_mosquito_health|instalacion_inicial|instalar_awscli|instalar_mqtt|mqtt_update_config|mqtt_update_pass|mqtt_server_diagnosis} [secret-name-inicial]"
+    echo "Uso: $0 {reparar_permisos|configurar_limpieza|configurar_logrotate|instalacion_inicial|instalar_awscli|instalar_mqtt|mqtt_update_config|mqtt_update_pass|mqtt_server_diagnosis|configurar_monitor|configurar_reinicio} [secret-name-inicial]"
     exit 1
     ;;
 esac
