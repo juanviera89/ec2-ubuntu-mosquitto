ae3450d4bc15403023985254da063dd1d96ea071
Juan Viera (PC Tarro) <juanviera89@gmail.com>
2025-03-12 22:28:35 -0300

    fix elevated_role call in update pass

--- scripts/mqtt_update_pass.sh
+++ scripts/mqtt_update_pass.sh
@@ -7,7 +7,7 @@ source "$DIR_NAME/instance_role.sh"
 SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id "$1" --query 'SecretString' --output text)
 
 # 3. Asumir rol elevado
-source "$DIR_NAME/elevated_role.sh "$1""
+source "$DIR_NAME/elevated_role.sh" "$1"
 "$DIR_NAME/mqtt_update_pass_sub.sh" "$SECRET_VALUE"
 sudo systemctl restart mosquitto
 echo "Mosquitto | pass file actualizada correctamente."
